138dbf393a32eeae7c89a596066bec704a22701e
Reisen09 <mcmabilangan@tip.edu.ph>
2026-03-18 10:27:13 +0800

    Fix BASE_DIR definition for dotenv

--- core/settings.py
+++ core/settings.py
@@ -15,10 +15,12 @@ import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv(BASE_DIR / '.env')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
+
+
 
 
 # Quick-start development settings - unsuitable for production
