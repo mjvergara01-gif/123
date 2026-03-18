094aa168a914bc0e2b9a71a4c76b6520575b8a96
Reisen09 <mcmabilangan@tip.edu.ph>
2026-03-18 10:43:15 +0800

    Fix crispy-forms version and upgrade pip

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
-#!/usr/bin/env bash 
-set -o errexit # exit on error 
-pip install -r requirements.txt 
-python manage.py collectstatic --no-input 
-python manage.py migrate 
+#!/usr/bin/env bash
+set -o errexit # exit on error
+
+# Upgrade pip
+python -m pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files and migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
